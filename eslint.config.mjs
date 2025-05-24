b8e625fb1902d261e1c0a1cc935b7f5bcbead984
Cheryl Yu <cherylworkshop@gmail.com>
2025-05-24 16:40:33 +0800

    Set up Prettier and ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,18 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+      'vue/multi-word-component-names': 'off',
+      'no-console': 'warn',
+      'no-debugger': 'warn'
+    },
+    ignores: ['node_modules', '.nuxt', 'dist']
+  }
 )
